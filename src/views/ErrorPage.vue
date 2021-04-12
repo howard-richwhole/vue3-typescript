701df3479f28aee5bea36b4d32311941d633fb1c
howard-richwhole <79359287+howard-richwhole@users.noreply.github.com>
2021-04-12 17:45:59 +0800

    Update ErrorPage.vue

--- src/views/ErrorPage.vue
+++ src/views/ErrorPage.vue
@@ -2,12 +2,12 @@
 div
   div {{ text }}
   div reactive 物件{{ obj }}
-  div reactive 物件內的值，直接取用{{ objCount }}
-  div 轉ref 後取用{{ refObjCount }}
+  div reactive 物件內的值，直接取用，不會更新，因為非ref - {{ objCount }}
+  div 將內值轉ref後取用，可更新{{ refObjCount }}
 </template>
 
 <script lang="ts">
-import { ref, reactive, toRefs, defineComponent } from 'vue'
+import { ref, reactive, toRefs, defineComponent, onMounted } from 'vue'
 import gsap from 'gsap'
 
 export default defineComponent({
@@ -15,12 +15,11 @@ export default defineComponent({
     const text = ref('hi')
     text.value += ' you'
     const obj = reactive({ count: 0 })
-    obj.count += 1
     const refObj = toRefs(obj)
     gsap.to(refObj.count, { duration: 10, value: 100 })
-    // setTimeout(() => {
-    //   refObj.count.value += 20
-    // }, 2000)
+    onMounted(() => {
+      obj.count++
+    })
     return { text, obj, objCount: obj.count, refObjCount: refObj.count }
   },
 })
